0aa0f29aea398cac5a0ed9e3abea227d3c48d97f
Carlos Estrada <carlos.deg02@hotmail.com>
2018-08-04 15:26:52 -0500

    Added test to main

--- RaspServer/src/com/comolaestaviendo/RaspServer.java
+++ RaspServer/src/com/comolaestaviendo/RaspServer.java
@@ -1,10 +1,18 @@
 package com.comolaestaviendo;
 
+import java.io.IOException;
+
 public class RaspServer {
 
     public static void main(String args[]) {
 
+        BluetoothServer bluetoothServer = new BluetoothServer();
 
+        try {
+            bluetoothServer.run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
