eaa484ad827d2dc8f6d2fa470c9f725cf949aea5
Carlos Estrada <carlos.deg02@hotmail.com>
2018-08-04 13:31:23 -0500

    Added server class and main class

---
+++ RaspServer/src/com/comolaestaviendo/RaspServer.java
@@ -0,0 +1,10 @@
+package com.comolaestaviendo;
+
+public class RaspServer {
+
+    public static void main(String args[]) {
+
+
+
+    }
+}
---
+++ RaspServer/src/com/comolaestaviendo/Server.java
@@ -0,0 +1,14 @@
+package com.comolaestaviendo;
+import java.net.*;
+
+
+public class Server {
+
+    private ServerSocket server;
+    private Socket socket;
+
+    public Server() {
+
+    }
+
+}
