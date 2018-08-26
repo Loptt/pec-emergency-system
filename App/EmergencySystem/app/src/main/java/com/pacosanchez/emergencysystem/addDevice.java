58ba1cb8b240f8504c9de8679c31563e1f9fe3ac
Paco Sanchez <paco.sanchez.ram@gmail.com>
2018-08-26 03:10:01 -0500

    Ya casi

--- App/EmergencySystem/app/src/main/java/com/pacosanchez/emergencysystem/addDevice.java
+++ App/EmergencySystem/app/src/main/java/com/pacosanchez/emergencysystem/addDevice.java
@@ -3,19 +3,27 @@ package com.pacosanchez.emergencysystem;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 public class addDevice extends AppCompatActivity {
 
-    private ViewPager mSlideViewPager;
-    private LinearLayout mDotLayout;
+
+    Button addDevice;
+
+    EditText raspName;
+    EditText raspIp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_device);
 
-        mSlideViewPager = findViewById(R.id.slideViewPager);
-        mDotLayout = findViewById(R.id.mDotLayout);
+        raspName = findViewById(R.id.raspName_et);
+        raspIp = findViewById(R.id.raspIp_et);
+
+        addDevice = findViewById(R.id.bAddRasp);
+
     }
 }
